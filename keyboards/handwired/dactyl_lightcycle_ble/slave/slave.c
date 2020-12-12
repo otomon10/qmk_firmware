af071402fc87d82c7d4c8fe425e1ce312d23f178
otomon10 <otomono@gmail.com>
2020-12-12 22:03:29 +0900

    Change: slave transitions to dfu mode only immediately after USB connection

--- keyboards/handwired/dactyl_lightcycle_ble/slave/slave.c
+++ keyboards/handwired/dactyl_lightcycle_ble/slave/slave.c
@@ -1,14 +1,11 @@
 
-#include "matrix.h"
-
 #include "app_ble_func.h"
 #include "bootloader.h"
-#include "wait.h"
-
+#include "matrix.h"
 #include "nrf.h"
 #include "nrf_power.h"
-
 #include "rgblight.h"
+#include "wait.h"
 
 bool has_usb(void);
 
@@ -26,7 +23,7 @@ void matrix_scan_user() {
     }
 
     /* If the slave has a USB connection, it will be forced into dfu mode */
-    if (cnt++ % 50 == 0 && has_usb()) {
+    if (cnt++ < 100 && has_usb()) {
         rgblight_sethsv_noeeprom(302, 255, 255);
         nrf_delay_ms(10);
         bootloader_jump();
