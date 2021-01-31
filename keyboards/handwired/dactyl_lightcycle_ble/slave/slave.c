09de512befd37baa0f4d619a05ed678a03acefe9
otomon10 <otomono@gmail.com>
2021-01-31 12:47:30 +0900

    Change: Improved Slave reconnect process

--- keyboards/handwired/dactyl_lightcycle_ble/slave/slave.c
+++ keyboards/handwired/dactyl_lightcycle_ble/slave/slave.c
@@ -2,6 +2,7 @@
 #include "app_ble_func.h"
 #include "ble_slave.h"
 #include "bootloader.h"
+#include "keymap_def.h"
 #include "keymaps/default/tps61291.h"
 #include "matrix.h"
 #include "nrf.h"
@@ -18,24 +19,33 @@ void matrix_init_user(void) {
 
 void matrix_scan_user() {
     static int cnt;
-    static bool init_rgblight = false;
+    static bool init_rgblight = true;
+    static bool init_connect_rgblight = true;
 
-    if (is_connected_master()) {
-        if (!init_rgblight) {
-            rgblight_enable_noeeprom();
-            nrf_delay_ms(10);  // need this
-            rgblight_sethsv_noeeprom(0, 0, 255);
-            nrf_delay_ms(500);
-            rgblight_sethsv_noeeprom(0, 0, 0);
-            nrf_delay_ms(10);  // need this
-            init_rgblight = true;
-        }
+    if (init_rgblight) {
+        rgblight_enable_noeeprom();
+        nrf_delay_ms(10);  // need this
+        init_rgblight = false;
     }
 
     /* If the slave has a USB connection, it will be forced into dfu mode */
-    if (cnt++ < 100 && has_usb()) {
+    if (cnt < 100 && has_usb()) {
         rgblight_sethsv_noeeprom(302, 255, 255);
         nrf_delay_ms(10);
         bootloader_jump();
     }
+
+    if (is_connected_master()) {
+        if (init_connect_rgblight) {
+            rgblight_sethsv_noeeprom(0, 0, 255);
+            nrf_delay_ms(500);
+            rgblight_sethsv_noeeprom(0, 0, 0);
+            nrf_delay_ms(10);  // need this
+            init_connect_rgblight = false;
+        }
+    } else {
+        init_connect_rgblight = true;
+    }
+
+    cnt++;
 }
